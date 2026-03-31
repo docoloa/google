4c0a7d10aa717af50ced9932851d8536ded33200
docoloa <goforreg@outlook.com>
2026-03-31 22:56:46 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,14 @@ const proxy = createProxyMiddleware({
   changeOrigin: true,
   secure: false,
   protocolRewrite: "http",
-  followRedirects: false
+  followRedirects: false,
+
+  // 强制伪装成浏览器，谷歌不会校验你
+  headers: {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    "Referer": "http://www.google.com.hk/",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+  }
 });
 
 module.exports = (req, res) => {
